002442fa7c110c5e259ae1245fa0e32059217436
verstage <68508962+verstage@users.noreply.github.com>
2021-01-18 01:02:52 +0100

    Add files via upload

---
+++ strona/js/main.js
@@ -0,0 +1,11 @@
+$('.scrollTo').on('click', function(e) {
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1000);
+    }
+});
