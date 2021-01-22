50a60177d5a3d2cd1c7b951d409307105a91cfbd
verstage <68508962+verstage@users.noreply.github.com>
2021-01-22 18:53:33 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+
+//Home page
+    //Stiky nav-bar
+
+    //When user srcoll the page, execute function
+    const navbar = document.getElementById("navbar");
+    const main = document.getElementById("main");
+    const sticky = navbar.offsetTop;
+    
+    function stickyClassFunction() {
+        if(window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+            
+        } else {
+            navbar.classList.remove("sticky");
+        }
+        return ;
+    }
+
+    window.onscroll = function () {
+    stickyClassFunction()
+    return;
+    };
+    /////////////////////////////////////////////////////
+
+    
+    
+// Mobile menu
+const hamburger = document.getElementById("hamburger");
+const mobileMenuContainer = document.getElementById("mobile-menu-container");
+const closeIconMobileMenu = document.getElementById("close-icon-mobile-menu");
+
+const mobileMenuSlide = () => {
+    mobileMenuContainer.classList.toggle("active");
+}
+
+hamburger.addEventListener("click", mobileMenuSlide);
+closeIconMobileMenu.addEventListener("click", mobileMenuSlide);
+
+
+
+
+
+// Srcolling button on in showcase home page
 $('.scrollTo').on('click', function(e) {
     if(this.hash !== '') {
         e.preventDefault();
@@ -5,7 +49,7 @@ $('.scrollTo').on('click', function(e) {
         const hash = this.hash;
 
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            scrollTop: $(hash).offset().top - 300
         }, 1000);
     }
 });
