e12c89ef0f8b0b4737a027dbbc39c817e08eba69
verstage <68508962+verstage@users.noreply.github.com>
2021-02-08 19:06:14 +0100

    dodana podstrona o mnie + slider i responsywność

---
+++ js/about-me.js
@@ -0,0 +1,58 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const carouselContainer = document.querySelector(".carousel-container");
+const prevBtnDiv = document.querySelector(".prevBtn");
+const nextBtnDiv = document.querySelector(".nextBtn");
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+let size = carouselImages[counter].clientWidth;
+
+
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Button Listeners
+
+nextBtn.addEventListener("click", () => {
+    if(counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    
+});
+prevBtn.addEventListener("click", () => {
+    if(counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    
+});
+
+carouselSlide.addEventListener("transitionend", () => {
+    if(carouselImages[counter].id === "lastClone"){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - 2;
+        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    }
+    if(carouselImages[counter].id === "firstClone"){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    }
+});
+
+carouselContainer.addEventListener("mouseenter", () => {
+    prevBtnDiv.classList.add("shadow-grey-effect-left");
+    nextBtnDiv.classList.add("shadow-grey-effect-right");
+    console.log("Hovered");
+});
+carouselContainer.addEventListener("mouseleave", () => {
+    prevBtnDiv.classList.remove("shadow-grey-effect-left");
+    nextBtnDiv.classList.remove("shadow-grey-effect-right");
+    console.log("Unhovered");
+});
